14c059c2da1082f7b3dd248f78918cd31dd50472
coderyu <657379242@qq.com>
2021-04-26 21:45:39 +0800

    2021/4/26

--- src/simple/ContainsNearbyDuplicate.java
+++ src/simple/ContainsNearbyDuplicate.java
@@ -1,5 +1,8 @@
 package simple;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 219. 存在重复元素 II
  *
@@ -51,4 +54,16 @@ public class ContainsNearbyDuplicate {
      *
      * 时间复杂度：O(n)，空间复杂度：O(min(n,k))
      * */
+
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
 }
